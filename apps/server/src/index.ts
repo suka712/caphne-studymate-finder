180c2909e8e68a90c44cdb1115059c4a85cec98c
Anh D Tran <tran3ah@mail.uc.edu>
2026-01-13 00:25:46 +0700

    Healthcheck implemented

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -6,7 +6,20 @@ const PORT = process.env.PORT
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from express' })
+  res.json({ message: 'Hello from Express!' })
+})
+
+app.get('/health', (req, res) => {
+  const uptimeSeconds = process.uptime()
+  const hours = Math.floor(uptimeSeconds / 3600)
+  const minutes = Math.floor((uptimeSeconds % 3600) / 60)
+  const seconds = Math.floor(uptimeSeconds % 60)
+
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+    uptime: `${hours}h ${minutes}m ${seconds}s`
+  })
 })
 
 app.listen(PORT, () => {
