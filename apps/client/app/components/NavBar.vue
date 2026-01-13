0d5e4850b3d98f0d6f093d743918c11fd35a513c
khiem <khiemnguyen.hye@gmail.com>
2026-01-13 16:48:48 +0700

    Scaffold navbar

---
+++ apps/client/app/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="sticky top-0">
+    <nav class="flex items-center mx-auto max-w-7xl px-4 py-4">
+      <div class="flex flex-1 justify-start">
+        <NuxtLink to="/" class="text-xl font-bold">
+          Caphne
+        </NuxtLink>
+      </div>
+      <div class="flex flex-1 gap-2 justify-center">
+        <Button>One button</Button>
+        <Button>Two button</Button>
+        <Button>Three button</Button>
+      </div>
+      <div class="flex flex-1 gap-2 justify-end">
+        <Button>
+          Language
+        </Button>
+        <Button>
+          Start now
+        </Button>
+      </div>
+    </nav>
+  </header>
+</template>
--- apps/client/app/layouts/default.vue
+++ apps/client/app/layouts/default.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="dark min-h-screen bg-background text-foreground">
-    <header class="border-b">
-      <nav class="mx-auto max-w-7xl px-4 py-4">
-        <NuxtLink to="/" class="text-xl font-bold">
-          Caphne
-        </NuxtLink>
-      </nav>
-    </header>
+    <NavBar />
     <main class="mx-auto max-w-7xl px-4 py-8">
       <slot />
     </main>
