25b3b00a80cab6105c64074d658032968720e14d
khiem <khiemnguyen.hye@gmail.com>
2026-01-14 12:42:26 +0700

    Adjust background focus color

--- apps/client/app/components/ui/navigation-menu/index.ts
+++ apps/client/app/components/ui/navigation-menu/index.ts
@@ -10,5 +10,5 @@ export { default as NavigationMenuTrigger } from "./NavigationMenuTrigger.vue"
 export { default as NavigationMenuViewport } from "./NavigationMenuViewport.vue"
 
 export const navigationMenuTriggerStyle = cva(
-  "bg-primary text-primary-foreground hover:bg-primary/90 group inline-flex h-9 w-max items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium hover:bg-primary/90 focus:bg-accent disabled:pointer-events-none disabled:opacity-50  focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1",
+  "bg-primary text-primary-foreground hover:bg-primary/90 group inline-flex h-9 w-max items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium hover:bg-primary/90 disabled:pointer-events-none disabled:opacity-50 outline-none transition-[color,box-shadow]",
 )
