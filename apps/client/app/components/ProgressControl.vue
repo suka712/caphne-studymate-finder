98e08c95495964da12a9cebed3f84571429151f8
khiem <khiemnguyen.hye@gmail.com>
2026-01-17 16:14:21 +0700

    Wire up navigation button

--- apps/client/app/components/ProgressControl.vue
+++ apps/client/app/components/ProgressControl.vue
@@ -1,9 +1,18 @@
+<script setup lang="ts">
+  defineProps<{
+    currentQuestion: Number
+    totalQuestions: Number
+    onNext: () => void
+    onPrevious: () => void
+  }>()
+</script>
+
 <template>
   <div class="fixed bottom-15 h-0.5 w-screen bg-muted-foreground/30"></div>
   <div class="flex bottom-0 fixed h-15 w-screen border-t">
     <div class="flex justify-between items-center w-2xl mx-auto">
       <div class="flex flex-1 justify-start">
-        <Button variant="ghost">
+        <Button variant="ghost" @click="onPrevious" :disabled="currentQuestion === 1">
           <Icon name="material-symbols:arrow-back-ios-rounded" /> Previous
         </Button>
       </div>
@@ -11,10 +20,11 @@
         Someshit
       </div>
       <div class="flex flex-1 justify-end">
-        <Button variant="ghost">
-          Next <Icon name="material-symbols:arrow-forward-ios-rounded" />
+        <Button variant="ghost" @click="onNext" :disabled="currentQuestion === totalQuestions">
+          Next
+          <Icon name="material-symbols:arrow-forward-ios-rounded" />
         </Button>
       </div>
-      </div>
+    </div>
   </div>
 </template>
